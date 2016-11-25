aabc8a42a50cbea91ac7814d0ce3255241b776ff
Henry Walters <henrywalters20@gmail.com>
2016-11-25 00:47:09 -0500

    Final touches

--- url_direct.rb
+++ url_direct.rb
@@ -967,3 +967,36 @@ end
 get '/thanks' do 
 	erb :thanks
 end
+post '/thanks' do 
+	if params[:home]
+		redirect '/'
+	end
+	if params[:login]
+		session["search"] = []
+		redirect '/login' 
+	end
+	if params[:register]
+		redirect '/register'
+	end
+	if params[:feedback]
+		redirect '/feedback'
+	end
+	if params[:logout]
+		logout(session[:user])
+		session[:user] = ""
+		session["search"] = []
+		redirect '/'
+	end
+	if params[:settings]
+		redirect '/settings'
+	end
+	if params[:post]
+		redirect '/post'
+	end
+	if params[:post_image]
+		redirect '/post/image'
+	end
+	if params[:search]
+		redirect "/search/#{params[:search_query].split(' ').join('-')}"
+	end
+end
